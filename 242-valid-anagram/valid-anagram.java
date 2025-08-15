a18c78eca1e5792bfc3f6b10d88193547990ed6b
Shreesh-404 <singh.shreesh.op@gmail.com>
2025-08-15 23:44:03 +0530

    Time: 5 ms (56.29%) | Memory: 42.9 MB (94.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,34 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-        Map<Character,Integer> hashMap1 = new HashMap<>();
-        Map<Character,Integer> hashMap2 = new HashMap<>();
+        if(s.length() != t.length()) return false;
+        int[] arr = new int[26];
+        for(int i = 0 ;i < s.length();i++){
+            arr[s.charAt(i) -'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+        for(int i = 0 ;i < 26;i++){
+            if(arr[i] != 0) return false;
+        }
+       
+       return true;
 
-        for (int i =0; i<s.length(); i++){
-            if (hashMap1.containsKey(s.charAt(i)))
-                hashMap1.replace(s.charAt(i),hashMap1.get(s.charAt(i))+1);
-            else
-                hashMap1.put(s.charAt(i),1);
-        }
-        for (int j =0; j<t.length(); j++){
-            if (hashMap2.containsKey(t.charAt(j)))
-                hashMap2.replace(t.charAt(j),hashMap2.get(t.charAt(j))+1);
-            else
-                hashMap2.put(t.charAt(j),1);
-        }
-        Set<Character> keys = hashMap1.keySet();
-        int i =0;
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            if (hashMap1.containsKey(ch) && hashMap2.containsKey(ch)){
-                if (hashMap1.get(ch).equals(hashMap2.get(ch))){
-
-                }else return false;
-                
-            }else if (hashMap1.containsKey(ch) == hashMap2.containsKey(ch)){
-                continue;
-            }else return false;
-        }
-        return true;
     }
 }
