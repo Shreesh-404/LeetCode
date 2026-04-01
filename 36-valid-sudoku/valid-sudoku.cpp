3a2a41aee4c71e8f9227a21919073df436bb6198
Shreesh-404 <singh.shreesh.op@gmail.com>
2026-04-01 15:18:30 +0530

    Time: 7 ms (27.36%) | Memory: 25.8 MB (8.14%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,35 +2,46 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
 
-        unordered_map<int, unordered_set<char>> col;
+        unordered_map<int,unordered_set<char>> col;
         unordered_map<int, unordered_set<char>> cube;
         
-        for (int i = 0; i < 9; i++) {
-
+        for (int i = 0; i < board.size(); i++)
+        {
             unordered_set<char> row;
 
-            for (int j = 0; j < 9; j++) {
+            for (int j = 0; j < board[i].size(); j++)
+            {
 
-                if(board[i][j] == '.') continue;
+                if(board[i][j] == '.'){
+                    continue;
+                }
 
                 char num = board[i][j];
 
-                // Row check
+                // Row Check
                 if(row.count(num)) return false;
                 row.insert(num);
 
-                // Column check
+                // Column Check
                 if(col[j].count(num)) return false;
                 col[j].insert(num);
 
-                // Cube check
-                int box = (i/3)*3 + j/3;
-
-                if(cube[box].count(num)) return false;
-                cube[box].insert(num);
+                // Cube Check
+                if (j < 3){
+                    if(cube[0].count(num)) return false;
+                    cube[0].insert(num);
+                }else if (j < 6){
+                    if(cube[1].count(num)) return false;
+                    cube[1].insert(num);
+                }else{
+                    if(cube[2].count(num)) return false;
+                    cube[2].insert(num);
+                }
+            }
+            if(i == 2 || i == 5 || i == 8){
+                cube.clear();
             }
         }
-
         return true;
     }
 };
