0bfaae1f4d5b47b98e199f909ecc207f5b7bcf78
Shreesh-404 <singh.shreesh.op@gmail.com>
2026-05-02 02:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (81.57%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,34 @@
+void findSum(int i, vector<int>& cand, vector<int>& temp, 
+             vector<vector<int>>& res, int target) {
+
+    if(target == 0){
+        res.push_back(temp);
+        return;
+    }
+
+    for(int j = i; j < cand.size(); j++) {
+
+        // skip duplicates
+        if(j > i && cand[j] == cand[j-1]) continue;
+
+        // pruning
+        if(cand[j] > target) break;
+
+        temp.push_back(cand[j]);
+        findSum(j + 1, cand, temp, res, target - cand[j]);
+        temp.pop_back();
+    }
+}
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+
+        vector<vector<int>> res;
+        vector<int> temp;
+
+        findSum(0, candidates, temp, res, target);
+        return res;
+    }
+};
