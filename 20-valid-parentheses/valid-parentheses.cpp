4164dde7319dfe8a71f8f2aed9ba73cb0daaf0a5
Shreesh-404 <singh.shreesh.op@gmail.com>
2026-07-20 06:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.22%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto it: s){
+            if(it == '(' || it == '{' || it == '['){
+                st.push(it);
+            }else{
+                if(st.empty()) return false;
+                char ch = st.top();
+                st.pop();
+                if(it == ')' && ch == '(' || it == '}' && ch == '{' || it == ']' && ch == '['){
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
